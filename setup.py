ffc8f0e6ea43763ccb551b9e04e1f3fefbbc0f35
mtoshi <mtoshi@outlook.com>
2020-05-18 00:29:38 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""setup.py"""
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+requires = []
+with open('requirements.txt', 'w') as _file:
+    _file.write('\n'.join(requires))
+
+setup(
+    name='django-graphql-jwt-demo',
+    version='0.0.1',
+    url='https://github.com/mtoshi/django-graphql-jwt-demo',
+    author='mtoshi',
+    author_email='mtoshi@outlook.com',
+    description='Experimental app.',
+    packages=find_packages(),
+    install_requires=requires,
+)
